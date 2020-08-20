bd6c8e49c645fa28b8495c496d8f444662dfddcf
JaeIn <sm810403@hotmail.com>
2020-08-19 19:38:39 -0700

    first commit
    html and css done

---
+++ app.js
@@ -0,0 +1,5 @@
+const searchBar = document.querySelector('.searchBar');
+const searchBtn = document.querySelector('.searchBtn');
+const lists = document.querySelector('.lists');
+const more = document.querySelector('.more');
+
